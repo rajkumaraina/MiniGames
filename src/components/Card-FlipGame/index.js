3d44fb3c692bfedd50fa376f6bb107cb0806f4eb
rajkumaraina <rajkumaraina3@gmail.com>
2024-04-22 12:10:49 +0000

    Mini Games

--- src/components/Card-FlipGame/index.js
+++ src/components/Card-FlipGame/index.js
@@ -8,10 +8,232 @@ import {RiCloseLine} from 'react-icons/ri'
 
 import './index.css'
 
+const cardsData = [
+  {
+    id: 0,
+    name: 'tiger',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-tiger-img.png',
+  },
+  {
+    id: 1,
+    name: 'lion',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-lion-img.png',
+  },
+  {
+    id: 2,
+    name: 'rat',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-rat-img.png',
+  },
+  {
+    id: 3,
+    name: 'hen',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-hen-img.png',
+  },
+  {
+    id: 4,
+    name: 'elephant',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-elephant-img.png',
+  },
+  {
+    id: 5,
+    name: 'buffalo',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-buffalo-img.png',
+  },
+  {
+    id: 6,
+    name: 'goat',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-goat-img.png',
+  },
+  {
+    id: 7,
+    name: 'zebra',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-zebra-img.png',
+  },
+  {
+    id: 8,
+    name: 'duck',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-duck-img.png',
+  },
+  {
+    id: 9,
+    name: 'pigeon',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-pigeon-img.png',
+  },
+  {
+    id: 10,
+    name: 'buffalo',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-buffalo-img.png',
+  },
+  {
+    id: 11,
+    name: 'zebra',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-zebra-img.png',
+  },
+  {
+    id: 12,
+    name: 'hen',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-hen-img.png',
+  },
+  {
+    id: 13,
+    name: 'rat',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-rat-img.png',
+  },
+  {
+    id: 14,
+    name: 'pigeon',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-pigeon-img.png',
+  },
+  {
+    id: 15,
+    name: 'lion',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-lion-img.png',
+  },
+  {
+    id: 16,
+    name: 'goat',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-goat-img.png',
+  },
+  {
+    id: 17,
+    name: 'tiger',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-tiger-img.png',
+  },
+  {
+    id: 18,
+    name: 'duck',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-duck-img.png',
+  },
+  {
+    id: 19,
+    name: 'elephant',
+    image:
+      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-elephant-img.png',
+  },
+]
+
+const CardFlip = props => {
+  const {
+    item,
+    CardSelected,
+    selected,
+    selectedId,
+    correctlySelected,
+    NoAction,
+  } = props
+  const {name, image, id} = item
+  const ParticularItem = cardsData.find(each => each.id === id)
+  let ImageElement
+  const Clicked = () => {
+    CardSelected(id)
+  }
+  if (correctlySelected.includes(ParticularItem)) {
+    ImageElement = image
+  } else {
+    let SameId = false
+    const selectedIdPresent = selectedId.includes(id)
+    if (selected.length > 0 && selectedIdPresent) {
+      SameId = true
+    }
+    if (SameId) {
+      ImageElement = image
+    } else {
+      ImageElement =
+        'https://res.cloudinary.com/dktgcdgar/image/upload/v1713775574/foot-print_1_v2i0ik.png'
+    }
+  }
+  return NoAction ? (
+    <li className="CardsListItem">
+      <img src={ImageElement} className="FootPrintImg" alt="animalImg" />
+    </li>
+  ) : (
+    <li className="CardsListItem" onClick={Clicked}>
+      <img src={ImageElement} className="FootPrintImg" alt="animalImg" />
+    </li>
+  )
+}
+
 class CardFlipGame extends Component {
-  state: {}
+  state = {
+    selected: [],
+    selectedId: [],
+    correctlySelected: [],
+    NoAction: false,
+  }
+
+  check = () => {
+    const {selected, correctlySelected} = this.state
+    console.log(selected)
+    const firstItem = selected[0]
+    const secondItem = selected[1]
+    console.log(firstItem)
+    console.log(secondItem)
+    if (firstItem.name === secondItem.name) {
+      console.log('Same')
+      this.setState(prevState => ({
+        correctlySelected: [
+          ...prevState.correctlySelected,
+          firstItem,
+          secondItem,
+        ],
+        selected: [],
+      }))
+    } else {
+      const timeOut = setTimeout(() => {
+        this.setState({selected: [], selectedId: [], NoAction: false})
+        clearTimeout(timeOut)
+      }, 2000)
+    }
+  }
+
+  BackButtonClicked = () => {
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  CardSelected = id => {
+    const {selected} = this.state
+    if (selected.length === 0) {
+      const SelectedItem = cardsData.find(each => each.id === id)
+      this.setState(prevState => ({
+        selected: [...prevState.selected, SelectedItem],
+        selectedId: [...prevState.selectedId, id],
+      }))
+    }
+    if (selected.length > 0) {
+      const SecondItem = cardsData.find(each => each.id === id)
+      this.setState(
+        prevState => ({
+          selected: [...prevState.selected, SecondItem],
+          selectedId: [...prevState.selectedId, id],
+          NoAction: true,
+        }),
+        this.check,
+      )
+    }
+  }
 
   render() {
+    const {selected, selectedId, correctlySelected, NoAction} = this.state
     return (
       <div className="CardFlipMainContainer">
         <div className="EmojiHomeBack">
@@ -91,7 +313,25 @@ class CardFlipGame extends Component {
             </div>
           </div>
         </div>
-        <h1 className="MMGameHeading">Card-Flip Game</h1>
+        <h1 className="CardFlipGameHeading">Card-Flip Memory Game</h1>
+        <div className="CardFlipScoreContainer">
+          <p className="CardFlipCount">Card flip count-00</p>
+          <p className="CardFlipTimer">Timer-02:00</p>
+          <p className="CardFlipScore">Score-00</p>
+        </div>
+        <ul className="CardFlipUnordered">
+          {cardsData.map(each => (
+            <CardFlip
+              item={each}
+              key={each.id}
+              CardSelected={this.CardSelected}
+              selected={selected}
+              selectedId={selectedId}
+              correctlySelected={correctlySelected}
+              NoAction={NoAction}
+            />
+          ))}
+        </ul>
       </div>
     )
   }
